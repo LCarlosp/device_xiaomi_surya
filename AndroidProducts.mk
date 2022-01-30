2707f26956ae061f294efb6eb25c2b6671c2b488
LCarlosp <laerciocp2830@gmail.com>
2022-01-30 10:18:43 -0300

    first commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 syberia
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/syberia_surya.mk
+
+COMMON_LUNCH_CHOICES := \
+    syberia_surya-user \
+    syberia_surya-userdebug \
+    syberia_surya-eng
